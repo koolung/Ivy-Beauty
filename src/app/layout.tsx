a148180fe64ac2fb0004a6c4c29951c733b08fe5
koolung <taeyun.koolung@gmail.com>
2025-11-04 10:59:32 -0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({ 
